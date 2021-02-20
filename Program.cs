8e9f985d032337ef0fe59146b3b912ea1a10031d
Ermogenes Palacio <epalacio@prodam.sp.gov.br>
2021-02-19 22:17:13 -0300

    Primeira versão do programa

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hello_world_pc1_2021_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
